e2f2be3f03945baff07a002fbe9834afcecc080c
Mohd Izhak <hein_htet_23@hotmail.com>
2023-09-03 16:48:44 +0800

    Authentication completed on server side

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,32 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import validator from "validator";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
 import * as jose from "jose";
 
 const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
   const bearerToken = req.headers.get("authorization");
-
-  if (!bearerToken) {
-    return NextResponse.json("Unauthorized request", { status: 401 });
+  let token = "";
+  let payload = { email: "", exp: 0 };
+  if (bearerToken) {
+    token = bearerToken.split(" ")[1];
   }
 
-  const token = bearerToken.split(" ")[1];
-
-  if (!token) {
-    return NextResponse.json("Unauthorized request", { status: 401 });
+  if (token) {
+    payload = jose.decodeJwt(token) as { email: string; exp: number };
   }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return NextResponse.json("Unauthorized request", { status: 401 });
-  }
-
-  const payload = jose.decodeJwt(token) as { email: string; exp: number };
 
   const user = await prisma.user.findMany({
     where: { email: payload.email },
